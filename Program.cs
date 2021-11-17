0fd8fd86e5f3a45202108df5fcd89351f55caaf4
titpe21 <89829607+titpe21@users.noreply.github.com>
2021-11-17 16:16:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ToDoList
 {
@@ -6,7 +9,39 @@ namespace ToDoList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //prepare a list to save data from file
+            List<string> toDoFromFile = new List<string>();
+            string toDoFilePath = @"C:\Users\opilane\samples\kt3\toDoList.txt";
+            //reading array of values from todo file and converting the array to list
+            toDoFromFile = File.ReadAllLines(toDoFilePath).ToList();
+
+            //display data from toDoList file
+            foreach(string task in toDoFromFile)
+            {
+                Console.WriteLine(task);
+            }
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a task? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if(userInput == 'y')
+                {
+                    Console.WriteLine("Enter a task:");
+                    string userTask = Console.ReadLine();
+                    toDoFromFile.Add(userTask);
+                    Console.WriteLine($"Task {userTask} added.");
+                } else
+                {
+                    Console.WriteLine("Take care!");
+                    loopActive = false;
+                }
+            }
+
+            File.WriteAllLines(toDoFilePath, toDoFromFile.ToArray());
         }
     }
 }
